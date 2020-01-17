469cf75041d39a801003c2e3e4e221ccb86ad5fb
ikalibrov <ilya.kalibrov@emergn.com>
2020-01-17 16:59:10 +0300

    Init

---
+++ public/src/js/feed.js
@@ -0,0 +1,31 @@
+var shareImageButton = document.querySelector('#share-image-button');
+var createPostArea = document.querySelector('#create-post');
+var closeCreatePostModalButton = document.querySelector('#close-create-post-modal-btn');
+
+function openCreatePostModal() {
+  createPostArea.style.display = 'block';
+  if (deferredPrompt.style.display = 'block') {
+      deferredPrompt.prompt();
+
+      deferredPrompt.userChoice.then(function(choiceResult) {
+        console.log(choiceResult.outcone);
+
+        if(choiceResult.outcome === 'dismissed') {
+          console.log('User cancelled installation');
+        }
+        else {
+          console.log('User added to homescreen');
+        }
+      });
+
+      deferredPrompt = null;
+  }
+}
+
+function closeCreatePostModal() {
+  createPostArea.style.display = 'none';
+}
+
+shareImageButton.addEventListener('click', openCreatePostModal);
+
+closeCreatePostModalButton.addEventListener('click', closeCreatePostModal);

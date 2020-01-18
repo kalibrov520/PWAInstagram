bf40640fdb1cc5ba95692c194b1f1dd9abecd8c2
ikalibrov <ilya.kalibrov@emergn.com>
2020-01-18 16:16:22 +0300

    Promise and fetch

--- public/src/js/feed.js
+++ public/src/js/feed.js
@@ -4,21 +4,20 @@ var closeCreatePostModalButton = document.querySelector('#close-create-post-moda
 
 function openCreatePostModal() {
   createPostArea.style.display = 'block';
-  if (deferredPrompt.style.display = 'block') {
-      deferredPrompt.prompt();
+  if (deferredPrompt) {
+    deferredPrompt.prompt();
 
-      deferredPrompt.userChoice.then(function(choiceResult) {
-        console.log(choiceResult.outcone);
+    deferredPrompt.userChoice.then(function(choiceResult) {
+      console.log(choiceResult.outcome);
 
-        if(choiceResult.outcome === 'dismissed') {
-          console.log('User cancelled installation');
-        }
-        else {
-          console.log('User added to homescreen');
-        }
-      });
+      if (choiceResult.outcome === 'dismissed') {
+        console.log('User cancelled installation');
+      } else {
+        console.log('User added to home screen');
+      }
+    });
 
-      deferredPrompt = null;
+    deferredPrompt = null;
   }
 }
 
--- public/sw.js
+++ public/sw.js
@@ -1,13 +1,14 @@
+
 self.addEventListener('install', function(event) {
-    console.log('[Service worker] Installing service worker...', event);
+  console.log('[Service Worker] Installing Service Worker ...', event);
 });
 
 self.addEventListener('activate', function(event) {
-    console.log('[Service worker] Activating service worker...', event);
-    return self.clients.claim();
+  console.log('[Service Worker] Activating Service Worker ....', event);
+  return self.clients.claim();
 });
 
 self.addEventListener('fetch', function(event) {
-    console.log('[Service worker] Fetching something...', event);
-    event.respondWith(fetch(event.request));
+  console.log('[Service Worker] Fetching something ....', event);
+  event.respondWith(fetch(event.request));
 });

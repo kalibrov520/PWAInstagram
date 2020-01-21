b4a98b0b9b36a1eef4480f94b615a07d59393468
ikalibrov <ilya.kalibrov@emergn.com>
2020-01-21 22:33:56 +0300

    push notification listen event added

--- functions/index.js
+++ functions/index.js
@@ -2,6 +2,7 @@
 var functions = require('firebase-functions');
 var admin = require('firebase-admin');
 var cors = require('cors')({origin: true});
+var webpush = require('web-push');
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
@@ -23,7 +24,26 @@ exports.storePostData = functions.https.onRequest(function(request, response) {
      image: request.body.image
    })
      .then(function() {
-       response.status(201).json({message: 'Data stored', id: request.body.id});
+       webpush.setVapidDetails('mailto: kalibrov520@gmail.com',
+        'BMz3yQnaEtJKgWYQ5ZGAnMenCylquuA5UUDSNswSAMMATrKFwQmGZEGfq8_tWuxNZNuTQtEvotj1tbpcporPXEQ',
+        'Qx7mHZCEh7Ue7YQcykt-CBjtNuAudqtihkF64IYLABs');
+        return admin.database().ref('subscriptions').once('value');
+     })
+     .then(function(subscriptions) {
+       subscriptions.forEach(function(sub){
+        var pushConfig = {
+          endpoint: sub.val().endpoint,
+          keys: {
+            auth: sub.val().keys.auth,
+            p256dh: sub.val().keys.p256dh
+          }
+        };
+        webpush.sendNotification(pushConfig, JSON.stringify({title: 'New Post', content: 'New post added'}))
+          .catch(function(err) {
+            console.log(err);
+          })
+       });
+       response.status(201).json({message: 'Data stored!', id: request.body.id})
      })
      .catch(function(err) {
        response.status(500).json({error: err});

826c430f35aa6ef2c180e1641e0bd5e8f5e8e6f2
ikalibrov <ilya.kalibrov@emergn.com>
2020-01-21 17:23:52 +0300

    cors fix

--- functions/index.js
+++ functions/index.js
@@ -1,27 +1,32 @@
-const functions = require('firebase-functions');
-const admin = require('firebase-admin');
-const cors = require('cors')({ origin: true });
+
+var functions = require('firebase-functions');
+var admin = require('firebase-admin');
+var cors = require('cors')({origin: true});
+
+// // Create and Deploy Your First Cloud Functions
+// // https://firebase.google.com/docs/functions/write-firebase-functions
+//
 
 var serviceAccount = require("./key/pwagram-key.json");
 
 admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
-    databaseURL: 'https://pwagram-8c84d.firebaseio.com/posts'
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: 'https://pwagram-8c84d.firebaseio.com/'
 });
 
-exports.storePostData = functions.https.onRequest((request, reponse) => {
-    cors(request, response, function() {
-        admin.database().ref('posts').push({
-            id: request.body.id,
-            title: request.body.title,
-            location: request.body.location,
-            image: request.body.image
-        })
-            .then(function() {
-                response.status(201).json({ message: 'Data stored', id: request.body.id })
-            })
-            .catch(function(ex) {
-                response.status(500).json({ error: ex }); 
-            });
-    });
+exports.storePostData = functions.https.onRequest(function(request, response) {
+ cors(request, response, function() {
+   admin.database().ref('posts').push({
+     id: request.body.id,
+     title: request.body.title,
+     location: request.body.location,
+     image: request.body.image
+   })
+     .then(function() {
+       response.status(201).json({message: 'Data stored', id: request.body.id});
+     })
+     .catch(function(err) {
+       response.status(500).json({error: err});
+     });
+ });
 });
